5f2ee2db5f91d07b50be26389ee8f1cfc61b5450
alextoasa <66973143+alextoasa@users.noreply.github.com>
2024-06-04 14:38:05 -0500

    jUNIO Preven

--- prevenImportRLt.php
+++ prevenImportRLt.php
@@ -127,8 +127,8 @@ if (isset($_POST['submit'])) {
                 elseif (empty($distinctMES[$i])) {
                     echo 'Registro ' . $i+2 . ' - ERROR, MES VACIO <br>';
                     $errors++;
-                }elseif ($distinctMES[$i]<>5) {
-                    echo 'Registro ' . $i+2 . ' - ERROR, EL VALOR MES ESTA FUERA DEL RANGO PERMITIDO (MAYO) <br>';
+                }elseif ($distinctMES[$i]<>6) {
+                    echo 'Registro ' . $i+2 . ' - ERROR, EL VALOR MES ESTA FUERA DEL RANGO PERMITIDO (JUNIO) <br>';
                     $errors++;
                 }
             }

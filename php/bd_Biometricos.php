834577c60221b8c290c582d7fae4a532de88162c
alextoasa <66973143+alextoasa@users.noreply.github.com>
2025-05-20 14:22:29 -0500

    Certificado de confianza en conexiones

--- php/bd_Biometricos.php
+++ php/bd_Biometricos.php
@@ -6,18 +6,8 @@ $usuario = "consultas";
 $nombreBaseDeDatos = "TimeSoft2";
 $rutaServidor = "10.10.100.12";
 
-
-/*
-$contraseña = "ppMEkwDrem";
-$usuario = "tswmabel";
-//$nombreBaseDeDatos = "dbTimeSoftWebAutomatic_MABEL";  ///produccion
-$nombreBaseDeDatos = "dbTimeSoftWebAutomatic_MABEL";
-$rutaServidor = "34.68.56.120";
-*/
-
-
 try {
-    $dbB = new PDO("sqlsrv:server=$rutaServidor;database=$nombreBaseDeDatos", $usuario, $contraseña);
+    $dbB = new PDO("sqlsrv:server=$rutaServidor;TrustServerCertificate=TRUE;database=$nombreBaseDeDatos", $usuario, $contraseña);
     $dbB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (Exception $e) {
     echo "Ocurrió un error con la base de datos DB BIOMETRICOS TIMESOFT:  " . $e->getMessage();

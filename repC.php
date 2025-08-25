0543739a48921acf53d402548dc5ad5c4c9fd1c3
alextoasa <66973143+alextoasa@users.noreply.github.com>
2025-08-25 16:12:12 -0500

    cambio para moviles

--- repC.php
+++ repC.php
@@ -338,8 +338,8 @@ set @idcab=?;
         ?>
         <tr>
             <td class="d-none d-md-table-cell"><?= $r->CodeBars ?></td>
-            <td><?= $r->ItemCode ?></td>
-            <td><?= $r->ItemName ?></td>
+            <td class="d-none d-md-table-cell"><?= $r->ItemCode ?></td>
+            <td><?= $r->ItemCode.' - '.$r->ItemName ?></td>
             <td class="d-none d-md-table-cell"><?= number_format($r->embalaje,0) ?></td>
             <td class="d-none d-md-table-cell"><?= number_format($r->OnHand,0) ?></td>
             <td class="d-none d-md-table-cell"><?= number_format($r->total_Transitoria_Tienda,0) ?></td>
